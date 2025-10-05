a421118538c549bdb064ab26868dcd67734db63f
Xyc| Artem K. <zoom.2000v@gmail.com>
2025-10-05 14:24:45 +0300

    Remove Registration component and its references from Home page

--- FID25/src/components/common/Form.tsx
+++ FID25/src/components/common/Form.tsx
@@ -165,7 +165,7 @@ export default function ContactForm() {
                             value={formData.name}
                             onChange={handleInputChange}
                             required
-                            placeholder='John Doe'
+                            placeholder='Ivan Franko'
                             className="w-full px-3 py-2 border border-yellow-500 rounded-md focus:outline-none focus:ring-2 focus:ring-amber-500 focus:border-amber-500"
                         />
                     </div>
--- FID25/src/components/pages/Home.tsx
+++ FID25/src/components/pages/Home.tsx
@@ -3,7 +3,6 @@ import { lazy } from "react";
 import Speakers from "../Home/Speakers.tsx";
 import Footer from "../common/Footer.tsx";
 const SectionBar = lazy(() => import('../common/SectionBar.tsx'))
-const Registration = lazy(() => import('../Home/Registration.tsx'))
 const About = lazy(() => import('../Home/About.tsx'))
 const Schedule = lazy(() => import('../Home/Schedule.tsx'))
 
@@ -18,8 +17,6 @@ function Home() {
             <Schedule />
             <SectionBar title="Speakers" />
             <Speakers />
-            <SectionBar title="Registration" />
-            <Registration />
             <Footer />
         </>
     );
