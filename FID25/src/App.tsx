7c3bfb399baeaa6e4eda1ba6700c7544d0299cd3
Xyc| Artem K. <zoom.2000v@gmail.com>
2025-11-01 18:43:58 +0200

    deploy

--- FID25/src/App.tsx
+++ FID25/src/App.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router"
 import Home from "./components/pages/Home.tsx"
-import Console from "./components/pages/Console.tsx"
-import FileSystemEditor from "./components/pages/FilleSystemEditor.tsx"
+
 
 
 
@@ -10,8 +9,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/console" element={<Console />} />
-        <Route path="/edit" element={<FileSystemEditor />} />
+
       </Routes>
     </>
   )
