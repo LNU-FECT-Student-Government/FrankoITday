1051e701523b0c0c02ce87f52a6a945bc90cf851
Xyc| Artem K. <zoom.2000v@gmail.com>
2025-11-01 18:55:46 +0200

    please

--- FID25/src/components/pages/Home.tsx
+++ FID25/src/components/pages/Home.tsx
@@ -3,7 +3,7 @@ import { lazy } from "react";
 //import Speakers from "../Home/Speakers.tsx";
 import Footer from "../common/Footer.tsx";
 const SectionBar = lazy(() => import('../common/SectionBar.tsx'))
-const About = lazy(() => import('../Home/About.tsx'))
+import About from "../Home/About.tsx";
 const Schedule = lazy(() => import('../Home/Schedule.tsx'))
 
 function Home() {
