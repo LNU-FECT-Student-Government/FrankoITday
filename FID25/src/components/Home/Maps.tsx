851f5f54fa4660719c0bf92a763bc79d73748f42
Stepan web2025-1 <zarichyy@gmail.com>
2025-11-06 09:20:43 +0200

    Update UI text and fix contributor name typo
    Changed 'поверх' to 'Floor' in Maps component for English consistency. Corrected contributor name from 'steiveof' to 'stevieof' in Footer component.

--- FID25/src/components/Home/Maps.tsx
+++ FID25/src/components/Home/Maps.tsx
@@ -72,7 +72,7 @@ function Maps() {
                 : inactiveButtonClass
                 }`}
             >
-              {floor} поверх
+              {floor} Floor
             </button>
           ))}
         </div>
